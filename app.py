1c617bc95495bf0be81557036bceddbdbc86925e
DoomerCreatine <doomercreatine@outlook.com>
2022-06-13 08:52:00 -0400

    Added plots

--- app.py
+++ app.py
@@ -52,10 +52,29 @@ for key, value in winners.items():
     
 leaderboard = pd.DataFrame(leaderboard)
 
+def update_graph():
+    fig = px.histogram(df, x = "date", title="Guesses per Stream")
+    fig.update_xaxes(
+
+    tickformat="%d %b %Y")
+    return fig
+
+def line_graph():
+    casket_data = df['casket'].tolist()
+    guess_data = df['guess'].tolist()
+    guess_lower = df['guess'].quantile(0.10)
+
+    guess_upper = df['guess'].quantile(0.90)
+    guess_data = np.where(guess_data < guess_lower, guess_lower, guess_data)
+
+    guess_data = np.where(guess_data > guess_upper, guess_upper, guess_data)
+    group_labels = ['casket', 'guess'] # name of the dataset
+    fig = ff.create_distplot([casket_data, guess_data], group_labels, show_hist=False, curve_type='normal', show_rug=False)
+    fig.layout.update(title='Guess and Casket Distributions')
+    return fig
 
 # Create the Dash app
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
-server = app.server
 
 # Set up the app layout
 app.layout = html.Div([
@@ -84,6 +103,18 @@ app.layout = html.Div([
         filter_options={'case': 'insensitive'},
         style_cell={'textAlign': 'center'}
     ),
+    
+    html.Div([
+        dcc.Graph(
+            id='guess-graph',
+            figure=update_graph(), style={'width': '49%', 'display': 'inline-block'}
+        ),
+        dcc.Graph(id='guess-line', figure=line_graph(), style={'width': '49%', 'display': 'inline-block'}),
+    ], style={'padding': '0 20'}),
+    html.Div([
+        dcc.Graph(id='guess-series', style={'width': '49%', 'display': 'inline-block'}),
+        dcc.Graph(id='guess-series-x', style={'width': '49%', 'display': 'inline-block'}),
+    ]),
     html.Br(),
     html.H3("All Data", style={
             'textAlign': 'center'
@@ -115,9 +146,55 @@ app.layout = html.Div([
 
 
 
+@app.callback(
+    Output('guess-series', 'figure'),
+    Input('guess-graph', 'clickData'))
+def update_x_timeseries(clickData):
+    idx = clickData['points'][0]['pointNumbers']
+    new_df = df.iloc[idx]
+    fig = px.histogram(new_df, x = "time", title="Guesses per Casket")
+    fig.update_xaxes(
+    tickformat="%H:%M")
+    return fig
+'''
+@app.callback(
+    Output('guess-line', 'figure'),
+    Input('guess-graph', 'clickData'))
+def update_line_timeseries(clickData):
+    idx = clickData['points'][0]['pointNumbers']
+    new_df = df.iloc[idx]
+    hist_data = new_df['casket'].tolist()
+    hist_data2 = new_df['guess'].tolist()
+    mean = np.median(np.array(hist_data), axis=0)
+    sd = np.std(np.array(hist_data), axis=0)
+
+    final_list = [x for x in hist_data if (x > mean - 2 * sd)]
+    final_list = [x for x in final_list if (x < mean + 2 * sd)]
+    group_labels = ['casket', 'guess'] # name of the dataset
+
+    fig = ff.create_distplot([final_list, hist_data2], group_labels, show_hist=False, curve_type='kde')
+    return fig
+    '''
+    
+
+@app.callback(
+    Output('guess-series-x', 'figure'),
+    Input('guess-series', 'clickData'))
+def update_xx_timeseries(clickData):
+    idx = clickData['points'][0]['pointNumbers']
+    new_df = df.iloc[idx]
+    fig = px.histogram(new_df, x = "guess", title = "Guesses Distribution")
+    fig.update_traces(xbins=dict( # bins used for histogram
+        start=0,
+        end=max(new_df['guess'])+100000,
+        size=100000
+    ))
+    return fig
+
 
 
 
 # Run local server
 if __name__ == '__main__':
+    app.title = "Hey_Jase Master Casket Dashboard"
     app.run_server(debug=False)
